bd7bab1aa78d1e28b7e7bb7176d8390bd3e7bb63
jeec98 <enriquezjarold4@gmail.com>
2025-04-01 17:11:05 -0500

    configuration user and DB mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/tesisdb'),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
-  imports: [UsersModule],
 })
 export class AppModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
   }
 
-  findAll() {
-    return `This action returns all users`;
+  findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string): Promise<User | null> {
+    return this.userModel.findById(id).exec();
   }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  
+  update(id: string, updateUserDto: UpdateUserDto): Promise<User | null> {
+    return this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true }).exec();
   }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  
+  remove(id: string): Promise<User | null> {
+    return this.userModel.findByIdAndDelete(id).exec();
   }
+  
 }
